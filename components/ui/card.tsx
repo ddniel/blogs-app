14c37a7285f7b89917fba0d63643cefb346ced7e
DannyBec <ddnielbec@outlook.com>
2024-07-10 20:04:50 -0400

    card design

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,6 +1,6 @@
 export default function Card() {
   return (
-    <div className="w-[400px] h-[350px] flex flex-col border border-neutral-200 rounded-md px-10 py-5 ">
+    <div className="w-[400px] h-[350px] flex flex-col border border-neutral-200 rounded-xl px-10 py-5 ">
       <h3 className="text-2xl">Title</h3>
       <div className="h-[2px] w-full bg-neutral-100"></div>
       <p>
@@ -8,9 +8,13 @@ export default function Card() {
         nemo, quod, magnam magni eius temporibus eos, voluptas voluptates? At
         provident consequatur distinctio dolorum eveniet unde? Saepe, ab.
       </p>
-      <div className="justify-self-end">
-        <button>Delete</button>
-        <button>Edit</button>
+      <div className="self-end">
+        <button className="px-3 py-1 border border-neutral-200 rounded-xl">
+          Delete
+        </button>
+        <button className="px-3 py-1 border border-neutral-200 rounded-xl">
+          Edit
+        </button>
       </div>
     </div>
   );
