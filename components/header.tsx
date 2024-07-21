7cb0b381baa7aa7dc01206c8d5a816eb51804cf9
DannyBec <ddnielbec@outlook.com>
2024-07-21 11:57:55 -0400

    Added middleware for /edit and /create

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,30 @@
 "use client";
+
 import { signOut } from "next-auth/react";
+import { isLoggedIn } from "../lib/auth";
 
 import Link from "next/link";
-import { useState } from "react";
 import { FaFacebookSquare, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [session, setSession] = useState(false);
+
+  useEffect(() => {
+    let res = false;
+    async function getSession() {
+      res = await isLoggedIn();
+      setSession(res);
+    }
+
+    getSession();
+  }, []);
+
   return (
     <div className=" fixed flex w-full z-10 backdrop-blur-sm text-foreground h-20 justify-between px-10 items-center border-b-2">
-      <h2 className="text-xxl font-bold">SOMEBODY</h2>
+      <Link href="/">
+        <h2 className="text-xxl font-bold">SOMEBODY</h2>
+      </Link>
       <div className="flex gap-5">
         <FaFacebookSquare
           size={30}
@@ -23,9 +39,11 @@ export default function Header() {
           className="text-neutral-500 cursor-pointer hover:text-foreground"
         />
 
-        <button onClick={() => signOut({ callbackUrl: "/" })}>Logout</button>
-
-        <Link href="/auth/signin">Login</Link>
+        {session ? (
+          <button onClick={() => signOut({ callbackUrl: "/" })}>Logout</button>
+        ) : (
+          <Link href="/auth/signin">Login</Link>
+        )}
       </div>
     </div>
   );
