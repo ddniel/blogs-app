97f8219fa785f6caeb9a18a2746a9dce54ede915
DannyBec <ddnielbec@outlook.com>
2024-07-21 22:57:06 -0400

    next-auth as never

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -36,6 +36,6 @@ export const authOptions = {
     signIn: "/auth/signin",
   },
 };
-export const handler = NextAuth(authOptions);
+export const handler = NextAuth(authOptions) as never;
 
 export { handler as GET, handler as POST };
