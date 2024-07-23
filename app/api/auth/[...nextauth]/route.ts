ed23926fd9380004b6561e952e06079ebbf73937
DannyBec <ddnielbec@outlook.com>
2024-07-23 11:27:58 -0400

    another config"

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -43,6 +43,6 @@ import { authOptions } from "@/lib/authOptions";
 //   },
 // };
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
