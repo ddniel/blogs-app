8b5b825f4896689034617388b26413ad47701a72
DannyBec <ddnielbec@outlook.com>
2024-07-23 17:59:36 -0400

    disabled seed route

--- app/seed/page.tsx
+++ app/seed/page.tsx
@@ -1,17 +1,18 @@
 import { seedPosts, seedUsers } from "@/scripts/seed";
 
 export default async function page() {
-  const users = await seedUsers();
-  const posts = await seedPosts();
+  // const users = await seedUsers();
+  // const posts = await seedPosts();
 
-  console.log(users, posts);
+  // console.log(users, posts);
 
-  if (
-    users?.message === "Users seeded succesfully." &&
-    posts?.message === "Seeded dummy posts succesfully."
-  ) {
-    return <div>Seeded succesfully.</div>;
-  } else {
-    return <div>Somethig went wrong</div>;
-  }
+  // if (
+  //   users?.message === "Users seeded succesfully." &&
+  //   posts?.message === "Seeded dummy posts succesfully."
+  // ) {
+  //   return <div>Seeded succesfully.</div>;
+  // } else {
+  //   return <div>Somethig went wrong</div>;
+  // }
+  return <div>Route disabled</div>;
 }
