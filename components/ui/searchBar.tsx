1501d3ed77e356e147a13b7500d9189582488448
DannyBec <ddnielbec@outlook.com>
2024-07-23 21:42:08 -0400

    wrapped searbar in suspense

--- components/ui/searchBar.tsx
+++ components/ui/searchBar.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { IoSearch } from "react-icons/io5";
+import { Suspense } from "react";
 
 export default function SearchBar() {
   const searchParams = useSearchParams();
@@ -22,18 +23,20 @@ export default function SearchBar() {
   }, 300);
 
   return (
-    <div className="border border-neutral-200 rounded-xl px-2 py-1 flex w-[350px] items-center gap-1 ml-[200px]">
-      <IoSearch />
-      <input
-        type="text"
-        id="search"
-        placeholder="Search posts..."
-        className="w-full outline-none bg-inherit"
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get("query")?.toString()}
-      />
-    </div>
+    <Suspense>
+      <div className="border border-neutral-200 rounded-xl px-2 py-1 flex w-[350px] items-center gap-1 ml-[200px]">
+        <IoSearch />
+        <input
+          type="text"
+          id="search"
+          placeholder="Search posts..."
+          className="w-full outline-none bg-inherit"
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          defaultValue={searchParams.get("query")?.toString()}
+        />
+      </div>
+    </Suspense>
   );
 }
